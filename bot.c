80ebac1be1bc19bd20b16a1d412135884a5195f7
Dien K Tran <dkt180000@utdallas.edu>
2018-10-28 09:08:29 -0500

    Updated code to save customer requests to a file, request.txt

--- bot.c
+++ bot.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <windows.h>
-#define SIZE 20
+#define SIZE 200
 #ifndef KEYWORDS_H
     #define KEYWORDS_H
     #include "keywords.h"
@@ -9,53 +9,56 @@
 
 int main()
 {
+    FILE* request;
     char chat[SIZE];
     helloMSG();
+    singleCommandReminder();
     while (1)
     {
         billMSG();
         spamMSG();
         simMSG();
         promoMSG();
+        requestMSG();
         exitMSG();
+        printf("> ");
         fgets(chat, SIZE, stdin);
-        while (strstr(chat, " ") != NULL) {
-            printf("Please do not enter any spaces\n");
-            helloMSG();
-            billMSG();
-            spamMSG();
-            simMSG();
-            promoMSG();
-            exitMSG();
-            fgets(chat, SIZE, stdin);
-        }
-        chat[strlen(chat)-1] = '\0';
-        if(strcmp(chat,"BILL")==0)
-        {
-            //opens link to BILL information
-            system("start https://support.t-mobile.com/docs/DOC-1723");
-        }
-        else if(strcmp(chat,"SPAM")==0)
-        {
-            //opens link to SPAM information
-            system("start https://support.t-mobile.com/docs/DOC-2747");
-        }
-        else if(strcmp(chat,"SIM")==0)
-        {
-            //opens link to SIM information
-            system("start https://support.t-mobile.com/docs/DOC-10162");
-        }
-        else if(strcmp(chat,"PROMO")==0)
-        {
-            //opens link to PROMO information
-            system("start https://www.t-mobile.com/offers/deals-hub");
-        }
-        else if(strcmp(chat,"EXIT")==0)
-        {
+        if (strstr(chat, " ") != NULL) {
+            request = fopen("request.txt", "a");
+            fprintf(request, "REQUEST: %s\n", chat);
+            printf("Thank you for sending your request. A representative will contact you shortly.\n");
             printf("Have a nice day.\n");
+            fclose(request);
             break;
+        } else {
+            chat[strlen(chat)-1] = '\0';
+            if(strcmp(chat,"BILL")==0)
+            {
+                //opens link to BILL information
+                system("start https://support.t-mobile.com/docs/DOC-1723");
+            }
+            else if(strcmp(chat,"SPAM")==0)
+            {
+                //opens link to SPAM information
+                system("start https://support.t-mobile.com/docs/DOC-2747");
+            }
+            else if(strcmp(chat,"SIM")==0)
+            {
+                //opens link to SIM information
+                system("start https://support.t-mobile.com/docs/DOC-10162");
+            }
+            else if(strcmp(chat,"PROMO")==0)
+            {
+                //opens link to PROMO information
+                system("start https://www.t-mobile.com/offers/deals-hub");
+            }
+            else if(strcmp(chat,"EXIT")==0)
+            {
+                printf("Have a nice day.\n");
+                break;
+            }
+            else printf("Unrecognized command\n");
         }
-        else printf("Unrecognized command\n");
     }
 }
 
--- keywords.h
+++ keywords.h
@@ -32,10 +32,18 @@ void promoMSG()
 
 void helloMSG()
 {
-    printf("Hello i am a helper bot here to guide you to links with instructions to help you fix your problems.\nWhat may I help you with?\n");
+    printf("Hello! I am a helper bot here to guide you to links with instructions to help you fix your problems.\nWhat may I help you with?\n");
 }
 
 void exitMSG()
 {
     printf("If you want to stop this conversation say EXIT\n");
+}
+
+void requestMSG() {
+    printf("But if you want to send a message to us, type it here. Include your name, email and your T-Mobile account number, and allow 24 hours for us to get back to you.\n");
+}
+
+void singleCommandReminder() {
+    printf("Please only enter a single command. If your input has spaces it will be treated as a request, and will be sent to a representative.\n");
 }
